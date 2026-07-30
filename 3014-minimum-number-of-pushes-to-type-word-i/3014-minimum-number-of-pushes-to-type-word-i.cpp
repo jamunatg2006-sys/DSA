a829299ec3a34640b8a906d7ac9afe6a547ccbc7
Jamuna T.G. <jamunatg2006@gmail.com>
2026-07-30 21:49:28 +0530

    Time: 0 ms (100%), Space: 8.3 MB (77.73%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minimumPushes(string word){
+        int ans=0;
+        for(int i=0;i<word.size();i++){
+            if(i<8){
+                ans+=1;
+            }
+            //ans=1+1+1+1+1+1+1+1
+            //2+2
+            //12 
+            else if (i<16)
+                ans+=2;
+            else if (i<24)
+                ans+=3;
+            else
+                ans+=4;
+        }
+        return ans;
+        
+    }
+};
